e48b71981697499405a8da39383e5a4ed180aaca
DumDereDum <batters21@yandex.ru>
2020-01-04 13:04:31 +0300

    Made finding of fonts

--- action.php
+++ action.php
@@ -1,6 +1,23 @@
-<?php 
-    $text = htmlspecialchars($_POST['text']);
-    $text = nl2br($text);
-    
-    echo $text; 
-?>
+<html>
+   <head>
+        <title>Manuscript</title>
+   </head>
+   <body>
+     <?php 
+          $text = htmlspecialchars($_POST['text']);
+          $text = nl2br($text);
+          $files = scandir('fonts');
+          
+          for ($i=2; $i<count($files); $i++)
+		{
+               $file[$i-2] = pathinfo('fonts/'.$files[$i], PATHINFO_FILENAME);
+          }
+          //print_r($files);echo "<br>";print_r($file);echo "<br>";
+          echo $text; 
+          for ($i=0; $i<strlen($_POST['lol']); $i++)
+		{
+               
+          }
+     ?>     
+    </body>   
+</html>
--- index.php
+++ index.php
@@ -1,12 +1,19 @@
-<form action="action.php"  method="post">
+<html>
+   <head>
+      <title>Manuscript</title>
+   </head>
+   <body>
+      <form action="action.php"  method="post">
 
- <p>Enter text:</p>
- <p> 
-    <textarea name="text">
-    </textarea>
- </p>
- <p>
-    <input type="submit" />
-</p>
-
-</form>
+      <p>Enter text:</p>
+      <p> 
+         <textarea name="text">
+         </textarea>
+      </p>
+      <p>
+         <input type="submit"/>
+      </p>
+      
+      </form>
+   </body>   
+</html>
