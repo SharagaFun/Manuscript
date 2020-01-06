d46d00dc77424dee70c22d1cae226faf8e890007
DumDereDum <batters21@yandex.ru>
2020-01-06 10:59:23 +0300

    Made finding of fonts

--- action.php
+++ action.php
@@ -13,11 +13,65 @@
                $file[$i-2] = pathinfo('fonts/'.$files[$i], PATHINFO_FILENAME);
           }
           //print_r($files);echo "<br>";print_r($file);echo "<br>";
-          echo $text; 
-          for ($i=0; $i<strlen($_POST['lol']); $i++)
-		{
+          //echo $text; 
+
+          $newText = '';
+          $sco = 0;
+          $nobring = 0;
+          $isbrd = true;
+          
+          for ($i=0; $i<strlen($text); $i++) {
+               echo 0;
+               while ((mb_substr($text,$i,1,'UTF-8')=="<"&&mb_strpos($text, '>', $i,'UTF-8'))
+                    ||(mb_substr($text,$i,1,'UTF-8')=="&"&&mb_strpos($text, ';', $i,'UTF-8'))
+                    ||(mb_substr($text,$i,1,'UTF-8')=="\n")) {
+                    
+                    echo ' - ';
+
+                    if (mb_substr($text,$i,1,'UTF-8')=="<"&&mb_strpos($text, '>', $i,'UTF-8')) {
+                         $oldi=$i;
+                         $i = mb_strpos($text, '>', $i,'UTF-8')+1;
+                         $newText.= mb_substr($text,$oldi,$i-$oldi,'UTF-8');
+                    }
+
+                    if (mb_substr($text,$i,1,'UTF-8')=="&"&&mb_strpos($text, ';', $i,'UTF-8')) {
+                         $oldi=$i;
+                         $i = mb_strpos($text, ';', $i,'UTF-8')+1;
+                         $newText.= mb_substr($text,$oldi,$i-$oldi,'UTF-8');
+                    }
+
+                    if (mb_substr($text,$i,1,'UTF-8')=="\n") {
+                         $GLOBALS['sco']++;
+                         $nobring = 0;
+                         $i++;
+                    }
+               }
+
+               echo 1;
+
+               $mrand = mb_substr($text,$i,1,'UTF-8')==' '?rand(-2,2):rand(1,4);
+               $font = "<font style=\"color:blue; font-size: ".rand(/*33,37*/18,24)."px; margin-right: ".$mrand."px\" face=\"".$file[rand(0,count($file)-1)]."\">";
+               $font1 = "</font>";
+               $newText.= $font.mb_substr($text,$i,1,'UTF-8').$font1; 
+               if ($nobring&&$nobring>37) {
+				$isbrd=false;
+			}
+			else {
+				$nobring++;
+			}
+               
+               echo 2;
+
+               if (!$isbrd&&mb_substr($text,$i,1,'UTF-8')==" "&&$nobring>37) {
+				$r1=rand(17,19); $r2=rand(48,65);
+				$isbrd=0;
+				$nobring=0;
+               }
+               
+               echo 3;
                
           }
+          echo "<div>".$newText."</div>";
      ?>     
     </body>   
 </html>
