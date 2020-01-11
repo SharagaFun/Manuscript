15e60ad02b2eed87e719d9948f97fba042c3ecdc
kolyanok <kolyanok@users.noreply.github.com>
2020-01-11 22:10:26 +0300

    Fixes to get it work

--- action.php
+++ action.php
@@ -1,17 +1,27 @@
 <html>
    <head>
         <title>Manuscript</title>
-   </head>
-   <body>
-     <?php 
-          $text = htmlspecialchars($_POST['text']);
+        <style>
+        	<?php
+        $text = htmlspecialchars($_POST['text']);
           $text = nl2br($text);
           $files = scandir('fonts');
           
           for ($i=2; $i<count($files); $i++)
-		{
+					{
                $file[$i-2] = pathinfo('fonts/'.$files[$i], PATHINFO_FILENAME);
+               
+               ?>@font-face {
+						font-family: "<?php echo $file[$i-2]; ?>";
+						src: url("fonts/<?php echo pathinfo('fonts/'.$files[$i], PATHINFO_BASENAME); ?>");
+					}<?
           }
+          ?>
+				</style>
+   </head>
+   <body>
+     <?php 
+          
           //print_r($files);echo "<br>";print_r($file);echo "<br>";
           //echo $text; 
 
@@ -21,12 +31,12 @@
           $isbrd = true;
           
           for ($i=0; $i<strlen($text); $i++) {
-               echo 0;
+               //echo 0;
                while ((mb_substr($text,$i,1,'UTF-8')=="<"&&mb_strpos($text, '>', $i,'UTF-8'))
                     ||(mb_substr($text,$i,1,'UTF-8')=="&"&&mb_strpos($text, ';', $i,'UTF-8'))
                     ||(mb_substr($text,$i,1,'UTF-8')=="\n")) {
                     
-                    echo ' - ';
+                  //  echo ' - ';
 
                     if (mb_substr($text,$i,1,'UTF-8')=="<"&&mb_strpos($text, '>', $i,'UTF-8')) {
                          $oldi=$i;
@@ -47,7 +57,7 @@
                     }
                }
 
-               echo 1;
+               //echo 1;
 
                $mrand = mb_substr($text,$i,1,'UTF-8')==' '?rand(-2,2):rand(1,4);
                $font = "<font style=\"color:blue; font-size: ".rand(/*33,37*/18,24)."px; margin-right: ".$mrand."px\" face=\"".$file[rand(0,count($file)-1)]."\">";
@@ -60,7 +70,7 @@
 				$nobring++;
 			}
                
-               echo 2;
+               //echo 2;
 
                if (!$isbrd&&mb_substr($text,$i,1,'UTF-8')==" "&&$nobring>37) {
 				$r1=rand(17,19); $r2=rand(48,65);
@@ -68,7 +78,7 @@
 				$nobring=0;
                }
                
-               echo 3;
+               //echo 3;
                
           }
           echo "<div>".$newText."</div>";
